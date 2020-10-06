9529c9af1d1d01c2c80551d40b92bc9afd6c60b2
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2020-10-06 13:07:32 +0530

    added new folder

---
+++ basic programming/pattern1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int length=20;
+    int width=5;
+    for(int j=0;j<width;j++){
+                for(int i=0;i<length;i++){
+            cout<<"*";
+        }
+        cout<<endl;
+
+    }
+
+
+return 0;
+}
+
