a90eee732a941fbeb6b3d342c5d485c0d355b8dc
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2020-10-11 11:48:19 +0530

    added fence codeforces

---
+++ codeforces/fence.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+//link->https://codeforces.com/contest/1422/problem/A
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long int a,b,c;
+        cin>>a>>b>>c;
+        cout<<(a+b+c)-1<<endl;
+    }
+return 0;
+}
