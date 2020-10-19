9044d72c4e07a26e3c21b66c7f063593156eebac
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2020-10-19 08:18:36 +0530

    Create derement.cpp

---
+++ codechef/cook off/oct cook off/derement.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+int32_t main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int l,r;
+        cin>>l>>r;
+       if(r>=2*l){
+           cout<<"-1"<<endl;
+       }
+       else{
+           cout<<r<<endl;
+       }
+
+
+        //s.substr(0, n);
+    }
+
+}
