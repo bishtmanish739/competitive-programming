12396e28f8791130a48e6e8c46b85b6d24469ab8
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2020-10-27 22:52:35 +0530

    Create marketingscheme.cpp

---
+++ codeforces/div 2/marketingscheme.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+int32_t main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int l,r;
+        cin>>l>>r;
+        if(2*l>r){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
+
