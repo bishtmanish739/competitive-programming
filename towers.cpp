2c78456c49dab88de4102e6a94b9dd2b392bac2a
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2020-10-28 21:26:22 +0530

    Create towers.cpp

---
+++ towers.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+int32_t main(){
+    int n;
+    cin>>n;
+    map<int ,int> mymap;
+    int towers=0;
+    for(int i=0;i<n;i++){
+        int k;
+        cin>>k;
+        if(mymap.find(k)!=mymap.end()){
+
+        }
+    }
+
+return 0;
+}
