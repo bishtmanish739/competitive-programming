7b603ea6f299edde65b0d60935224221063b1507
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2020-11-01 11:08:51 +0530

    Create andPlusOr.cpp

---
+++ codechef/lunchtime/octoberlunchtime/andPlusOr.cpp
@@ -0,0 +1,16 @@
+//link-https://www.codechef.com/LTIME89B
+//link=https://www.codechef.com/LTIME89B/problems/ANDOR
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    unsigned long long int x;
+	    cin>>x;
+	    cout<<0<<" "<<x<<endl;
+	}
+	return 0;
+}
