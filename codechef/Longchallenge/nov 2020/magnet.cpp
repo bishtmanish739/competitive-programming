33e7bd5ecab209817e3453b3aa483be0a734b9f9
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2020-11-17 10:45:33 +0530

    Update magnet.cpp

--- codechef/Longchallenge/nov 2020/magnet.cpp
+++ codechef/Longchallenge/nov 2020/magnet.cpp
@@ -13,9 +13,9 @@ int main(){
         cin>>n;
         int k;
         cin>>k;
-        string s;
-        cin>>s;
-        int ans=0;
+        string initialstring;
+        cin>>initialstring;
+        int finalans=0;
         int totalmagnet=0;
         vector<int>iindex;
         vector<int> mindex;
@@ -26,24 +26,25 @@ int main(){
         int currsheet=0;
        for(int i=0;i<n;i++){
             visitedarr[i]=false;
-            if(s[i]=='X'){
+            if(initialstring[i]=='X'){
                 vector<int>emptyvector;
             iindex=emptyvector;
             mindex=emptyvector;
             currsheet=0;
+           // cout<<mindex.size()<<endl;
 
 
 
 
             }
-            if(s[i]=='M'){
+            if(initialstring[i]=='M'){
                 mindex.push_back(i);
             }
-            if(s[i]=='I'){
+            if(initialstring[i]=='I'){
                 iindex.push_back(i);
 
             }
-            if(s[i]==':'){
+            if(initialstring[i]==':'){
                 currsheet++;
                sheettill.push_back(currsheet);
             }
@@ -53,13 +54,13 @@ int main(){
             if(iindex.size()>0&&mindex.size()>0){
                 int currpower=k+1;
 
-                if(s[i]=='I'){
+                if(initialstring[i]=='I'){
                         for(int j=0;j<mindex.size();j++){
                             currpower=k+1;
                      currpower-=abs(i-mindex[j]);
                     currpower=currpower-(sheettill[i]-sheettill[mindex[j]]);
                      if(currpower>0){
-                        ans++;
+                        finalans++;
                         mindex.erase(mindex.begin()+j);
                         iindex.erase(iindex.begin()+iindex.size()-1);
 
@@ -68,13 +69,13 @@ int main(){
                      }
                     }
                 }
-                if(s[i]=='M'){
+                if(initialstring[i]=='M'){
                         for(int j=0;j<iindex.size();j++){
                             currpower=k+1;
                      currpower-=abs(i-iindex[j]);
                      currpower=currpower-(sheettill[i]-sheettill[iindex[j]]);
                      if(currpower>0){
-                        ans++;
+                        finalans++;
                         mindex.erase(mindex.begin()+mindex.size()-1);
                         iindex.erase(iindex.begin()+j);
 
@@ -90,7 +91,7 @@ int main(){
             }
 
            }
-        cout<<ans<<endl;
+        cout<<finalans<<endl;
 
     }
 
