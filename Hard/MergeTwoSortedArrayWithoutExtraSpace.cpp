f0c11fac05d4586d04f233bf435335e46d05c494
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2020-12-19 14:13:32 +0530

    Create MergeTwoSortedArrayWithoutExtraSpace.cpp

---
+++ Hard/MergeTwoSortedArrayWithoutExtraSpace.cpp
@@ -0,0 +1,80 @@
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+
+
+int nextGap(int gap)
+{
+    if (gap <= 1)
+        return 0;
+    return (gap / 2) + (gap % 2);
+}
+void merge(int arr1[], int arr2[], int n, int m) 
+{ 
+     int i, j, gap = n + m;
+    for (gap = nextGap(gap); 
+         gap > 0; gap = nextGap(gap)) 
+    {
+        // comparing elements in the first array.
+        for (i = 0; i + gap < n; i++)
+            if (arr1[i] > arr1[i + gap])
+                swap(arr1[i], arr1[i + gap]);
+ 
+        // comparing elements in both arrays.
+        for (j = gap > n ? gap - n : 0; 
+             i < n && j < m;
+             i++, j++)
+            if (arr1[i] > arr2[j])
+                swap(arr1[i], arr2[j]);
+ 
+        if (j < m) {
+            // comparing elements in the second array.
+            for (j = 0; j + gap < m; j++)
+                if (arr2[j] > arr2[j + gap])
+                    swap(arr2[j], arr2[j + gap]);
+        }
+    }
+    
+} 
+
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    int arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    
+	    merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            printf("%d ", arr1[i]); 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    printf("%d ", arr2[i]); 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+  // } Driver Code Ends
